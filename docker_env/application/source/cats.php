2a9fc595c3b412de1832c5b78aec2913c39fce40
Nicolas Catalano <nicolascatalano1@gmail.com>
2024-07-15 16:48:08 +0200

    Mes premiers pas en php et je sais lacer mes chaussures :)

---
+++ docker_env/application/source/cats.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Hello mon grand :D! <br> Disons que ceci est la nouvelle page avec du contenu et tout et tout";
+	echo "<br><br>";
+	echo '<img src="koala.jpeg" border="0" /></div> ';
+
+?>
---
+++ docker_env/application/source/index.php
@@ -0,0 +1,6 @@
+<?php
+	echo "Hello mon grand :D! <br>";
+	echo "<br><br>";
+	echo '<img src="koala.jpeg" border="0" /></div> ';
+
+?>
